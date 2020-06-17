c3e221864ed00308f32ffa292a9ddb57ff1a3bc6
andreamaille <andrea.maille@gmail.com>
2020-06-16 22:58:36 -0400

    Added slider

--- js/app.js
+++ js/app.js
@@ -1,9 +1,33 @@
 import '../styles/style.scss';
+
 import { get } from 'https';
+// import 'slick-carousel/slick/slick-theme.css';
+
+const $ = require('jquery');
+require('jquery');
+require('slick-carousel');
 
 const app = {};
 
 app.apiKey = '1f645de0-a084-11ea-8c61-ad3e71f18257';
+
+app.getArt = () => {
+  const getArt = fetch(
+    `https://api.harvardartmuseums.org/object?size=50&classification=Paintings&apikey=${app.apiKey}`
+  );
+
+  getArt
+    .then(response => response.json())
+    .then(response => {
+      const allArtwork = response.records;
+      const artwork = allArtwork.filter(
+        item =>
+          item.primaryimageurl !== null && item.primaryimageurl !== undefined
+      );
+      console.log(artwork);
+      app.displayImages(artwork);
+    });
+};
 
 app.getArtByColor = selectedColor => {
   const getArt = fetch(
@@ -21,19 +45,27 @@ app.getArtByColor = selectedColor => {
 };
 
 app.displayImages = artwork => {
-  const imageContainer = document.querySelector('.gallery');
+  $('.gallery-slider').slick('slickRemove', null, null, true);
+  // const imageContainer = document.querySelector('.gallery');
 
   artwork.forEach(item => {
+
     const img = document.createElement('img');
     img.src = item.primaryimageurl;
-    imageContainer.appendChild(img);
+
+    $('.gallery-slider').slick(
+      'slickAdd',
+      `<div><img src='${item.primaryimageurl}'></div>`
+    );
   });
+
+  // $('.gallery')[0].slick.refresh();
 };
 
-app.clearCanvas = () => {
-  const imageContainer = document.querySelector('.gallery');
-  imageContainer.innerHTML = '';
-};
+// app.clearCanvas = () => {
+//   const imageContainer = document.querySelector('.gallery');
+//   imageContainer.innerHTML = '';
+// };
 
 app.selectedColor = e => {
   const selectedColor = e.target.dataset.color;
@@ -41,17 +73,28 @@ app.selectedColor = e => {
 };
 
 const colorOptions = document.getElementsByClassName('color-option');
-// console.log(colorOptions);
 
 for (const color of colorOptions) {
   color.addEventListener('click', function(e) {
-    app.clearCanvas();
+    // app.clearCanvas();
     app.selectedColor(e);
   });
 }
-// colorOptions.forEach(color => {
 
-//   // button
-// });
+app.init = () => {
+  app.getArt();
+};
 
-// app.init();
+
+$(document).ready(function() {
+  app.init();
+
+  $('.gallery-slider').slick({
+    dots: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: true,
+    autoplaySpeed: 2000,
+  });
+});
