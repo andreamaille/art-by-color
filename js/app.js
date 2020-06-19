b0bc81b3b6c9e094163643aa38e405e87cf019ed
andreamaille <andrea.maille@gmail.com>
2020-06-19 00:04:45 -0400

    Inital set-up of modal

--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
 import '../styles/style.scss';
 
 import { get } from 'https';
+
+const app = {};
 // import 'slick-carousel/slick/slick-theme.css';
 
 const $ = require('jquery');
 require('jquery');
 require('slick-carousel');
 
-const app = {};
-
 app.apiKey = '1f645de0-a084-11ea-8c61-ad3e71f18257';
+
+app.state = {
+  currentDisplay: [],
+};
 
 app.getArt = () => {
   const getArt = fetch(
@@ -25,7 +29,10 @@ app.getArt = () => {
           item.primaryimageurl !== null && item.primaryimageurl !== undefined
       );
       console.log(artwork);
+
       app.displayImages(artwork);
+
+      app.state.currentDisplay = artwork;
     });
 };
 
@@ -39,8 +46,9 @@ app.getArtByColor = selectedColor => {
     .then(response => {
       const allArtwork = response.records;
       const artwork = allArtwork.filter(item => item.primaryimageurl !== null);
-      console.log(artwork);
+
       app.displayImages(artwork);
+      app.state.currentDisplay = artwork;
     });
 };
 
@@ -74,6 +82,8 @@ app.displayImages = artwork => {
       currentSlide = [];
     }
   }
+
+  app.selectedImg();
 };
 
 app.selectedColor = e => {
@@ -82,7 +92,7 @@ app.selectedColor = e => {
 };
 
 const colorOptions = document.getElementsByClassName('color-option');
-
+// console.log(colorOptions);
 for (const color of colorOptions) {
   color.addEventListener('click', function(e) {
     // app.clearCanvas();
@@ -90,9 +100,51 @@ for (const color of colorOptions) {
   });
 }
 
+app.selectedImg = () => {
+  const images = document.querySelectorAll('img');
+
+  // console.log(images);
+  for (const image of images) {
+    image.addEventListener('click', function(e) {
+      // app.clearCanvas();
+
+      app.paintingInfo(e);
+    });
+  }
+};
+
+app.paintingInfo = e => {
+  const selectedPainting = app.state.currentDisplay.find(
+    painting => painting.primaryimageurl === e.target.src
+  );
+
+  app.createModal(selectedPainting);
+};
+
+app.createModal = selectedPainting => {
+  const galleryContainer = document.querySelector('.gallery');
+  const modal = document.createElement('div');
+  modal.className = 'modal';
+
+  modal.innerHTML = `
+    <button onclick="window.closeModal()">˟</button>
+    <div><img src='${selectedPainting.primaryimageurl}'>
+    <p>${selectedPainting.title}</p>`;
+  // console.log(selectedPainting.title);
+
+  galleryContainer.appendChild(modal);
+  // console.log(selectedPainting.primaryimageurl);
+};
+
+window.closeModal = e => {
+  const galleryContainer = document.querySelector('.gallery');
+  const modal = document.querySelector('.modal');
+  galleryContainer.removeChild(modal);
+};
+
 app.init = () => {
   app.getArt();
-
+  // app.selectedImg();
   $('.gallery-slider').slick({
     dots: false,
     slidesToShow: 5,
